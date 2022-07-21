d3ef7bc92f953fd7e5e9ee899d6ac6a997281e0d
EduardKhromtsov <109735904+EduardKhromtsov@users.noreply.github.com>
2022-07-21 15:49:16 +0300

    Add files via upload

---
+++ Beginner/1.cpp
@@ -0,0 +1,24 @@
+﻿// Составить программу, которая будет считывать введённое пятизначное число.После чего, каждую цифру этого числа необходимо вывести в новой строке.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int iNumber;
+    cout << "Enter a five-digit number: ";
+    cin >> iNumber;
+
+    if (iNumber < 10000 || iNumber > 99999)
+    {
+        cout << "You entered the wrong number, try again" << endl;
+    }
+    else
+    {
+        cout << "First number is " << "\t" << iNumber / 10000 << endl;
+        cout << "Second number is " << "\t" << (iNumber / 1000) % 10 << endl;
+        cout << "Third number is " << "\t" << (iNumber / 100) % 10 << endl;
+        cout << "Fourth number is " << "\t" << (iNumber / 10) % 10 << endl;
+        cout << "Fifth number is " << "\t" << (iNumber / 1) % 10 << endl;
+    }
+}
