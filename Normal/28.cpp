ecfdbc3d9bb1a01262f7f23ca8fdc84f50edb2df
EduardKhromtsov <109735904+EduardKhromtsov@users.noreply.github.com>
2022-08-05 17:05:11 +0300

    Add files via upload

---
+++ Normal/28.cpp
@@ -0,0 +1,43 @@
+﻿// Написать функцию, подсчитывающую среднее арифметическое целого массива. Предварительно объявив и, при необходимости, инициализировав переменные.
+
+#include <iostream>
+#include <ctime>
+
+double Average(const int* const pArr, const int Size)
+{
+    int iSum = 0;
+
+    for (int i = 0; i < Size; ++i)
+    {
+        iSum += pArr[i];
+    }
+
+    return (double)iSum / Size;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    srand(time(NULL));
+
+    int iSize;
+
+    std::cout << "Укажите количество элементов массива: ";
+    std::cin >> iSize;
+
+    int* pArray = new int[iSize];
+
+    for (int i = 0; i < iSize; ++i)
+    {
+        pArray[i] = rand() % 100;
+        std::cout << pArray[i] << " ";
+    }
+
+    std::cout << "\n";
+
+    std::cout << "Среднее арифметическое целого массива = " << Average(pArray, iSize) << "\n";
+
+    delete[] pArray;
+}
+
+
