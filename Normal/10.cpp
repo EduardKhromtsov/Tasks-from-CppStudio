bad0a52c9a65b3d19cc4eacca839264796783bd5
EduardKhromtsov <109735904+EduardKhromtsov@users.noreply.github.com>
2022-08-09 17:36:04 +0300

    Add files via upload

---
+++ Normal/10.cpp
@@ -0,0 +1,30 @@
+﻿// Подсчитать количество повторений элементов, заданного множества символов, во введенной строке.
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    unsigned short int iCounter = 1;
+
+    std::string sString;
+    
+    std::cout << "Введите строку: ";
+    std::getline(std::cin, sString);
+
+    
+    for (int i = 0; i < sString.size(); ++i)
+    {
+        for (int j = 0; j < sString.size(); ++j)
+        {
+            if (j != i && sString[i] == sString[j])
+                ++iCounter;
+        }
+        std::cout << sString[i] << " = " << iCounter << "\n";
+        iCounter = 1;
+    }
+}
+
+
