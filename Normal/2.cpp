37358946cee1715336152e33a9953751bda395d5
EduardKhromtsov <109735904+EduardKhromtsov@users.noreply.github.com>
2022-08-12 21:17:46 +0300

    Add files via upload

---
+++ Normal/2.cpp
@@ -0,0 +1,25 @@
+﻿// Используя показатели функции вычислить цепную дробь. Количество элементов дроби надо задавать с клавиатуры.
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    unsigned short int iNumber;
+
+    float fY = 1.0;
+
+    std::cout << "Введите количество элементов дроби 1+1/(1+1/(1+1/(1+1/…))): ";
+    std::cin >> iNumber;
+
+    for (int i = 0; i < iNumber; ++i)
+    {
+        fY = 1 + 1 / fY;
+    }
+
+    std::cout << "Результат: " << fY << "\n";
+
+}
+
+
