588531a9e923294a197fc12207c3366e446bd4ad
EduardKhromtsov <109735904+EduardKhromtsov@users.noreply.github.com>
2022-08-19 14:37:49 +0300

    Add files via upload

---
+++ Normal/17.cpp
@@ -0,0 +1,44 @@
+﻿// Поменять согласные буквы на гласные во введенной пользователем строке, а гласные — на соответствующий ASCII код, используя функции.
+
+#include <iostream>
+#include <ctime>
+
+void Search(const char *const Str, const int iSize);
+
+int main()
+{
+    srand(time(NULL));
+
+    char cString[100];
+
+    std::cout << "Enter the string of characters: ";
+    std::cin.getline(cString, 100);
+
+    std::cout << "Result: ";
+    
+    Search(cString, strlen(cString));
+
+    return 0;
+}
+
+void Search(const char *const Str, const int iSize)
+{
+    const char *cVowels = "AEIOUYaeiouy";
+    const char *cConsonants = "BCDFGHJKLMNPQRSTVWXZbcdfghjklmnpqrstvwxz";
+
+    for (int i = 0; i < iSize; ++i)
+    {
+        if (Str[i] == ' ')
+        {
+            std::cout << " ";
+            continue;
+        }
+
+        if (strchr(cConsonants, Str[i]) == NULL)
+            std::cout << int(Str[i]) << " ";
+        else
+            std::cout << cVowels[rand() % 12] << " ";
+    }
+    std::cout << "\n";
+}
+
