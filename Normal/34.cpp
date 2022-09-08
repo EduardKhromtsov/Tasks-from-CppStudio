0ac0892a58109947757d052c4644e149078dc6d2
EduardKhromtsov <109735904+EduardKhromtsov@users.noreply.github.com>
2022-09-08 13:44:39 +0300

    Add files via upload

---
+++ Normal/34.cpp
@@ -0,0 +1,49 @@
+﻿// Напишите функцию, которая принимает два целых числа n и k и возвращает число, содержащее k первых цифр числа n.
+
+#include <iostream>
+#include <cmath>
+
+void FindNumberSize(unsigned int iNumber, unsigned short int &iNumberSize)
+{
+    while (iNumber /= 10)
+        ++iNumberSize;
+}
+
+int DisplayDigit(const unsigned int iNumber, const unsigned short int iDigit, const unsigned short int iNumberSize)
+{
+    if (iDigit > iNumberSize || iDigit <= 0)
+        throw std::exception("Количество разрядов указано неверно.");
+
+    return iNumber / (int)pow(10, iNumberSize - iDigit);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    unsigned int iNumber;
+    unsigned short int iNumberSize = 1, iDigit;
+
+    std::cout << "Введите число: ";
+    std::cin >> iNumber;
+
+    FindNumberSize(iNumber, iNumberSize);
+
+    std::cout << "Укажите, сколько разрядов нужно показать: ";
+    std::cin >> iDigit;
+
+    try
+    {
+        std::cout << DisplayDigit(iNumber, iDigit, iNumberSize);
+    }
+    catch (const std::exception &ex)
+    {
+        std::cout << ex.what();
+    }
+
+    std::cout << "\n";
+
+    return 0;
+}
+
+
